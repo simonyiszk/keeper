4186d579c4568401b1ab232fc3f4a3afa5aa0cf9
varkokonyi <varkokonyi@gmail.com>
2020-06-25 18:24:08 +0200

    Add support controllers

--- src/main/java/com/sem/keeper/repo/LoanRepository.java
+++ src/main/java/com/sem/keeper/repo/LoanRepository.java
@@ -3,6 +3,8 @@ package com.sem.keeper.repo;
 import com.sem.keeper.entity.DeviceEntity;
 import com.sem.keeper.entity.LoanEntity;
 import com.sem.keeper.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +16,11 @@ public interface LoanRepository extends PagingAndSortingRepository<LoanEntity, L
 
     List<LoanEntity> findByElvitte(UserEntity elvitte);
 
+    Page<LoanEntity> findByElvitte(UserEntity elvitte, Pageable pageRequest);
+
     //List<LoanEntity> findByDeviceEntityAndVisszavetteIsNullOrderByTakeDate(DeviceEntity deviceEntity);
 
     List<LoanEntity> findByVisszavetteIsNullOrderByTakeDate();
+
+    Page<LoanEntity> findByVisszavetteIsNullOrderByTakeDate(Pageable pageable);
 }
--- src/main/java/com/sem/keeper/web/AjaxController.java
+++ src/main/java/com/sem/keeper/web/AjaxController.java
@@ -1,7 +1,13 @@
 package com.sem.keeper.web;
 
 import com.sem.keeper.entity.DeviceEntity;
+import com.sem.keeper.entity.LoanEntity;
+import com.sem.keeper.entity.LoanRequestEntity;
+import com.sem.keeper.entity.UserEntity;
 import com.sem.keeper.repo.DeviceRepository;
+import com.sem.keeper.repo.LoanRepository;
+import com.sem.keeper.repo.LoanRequestRepository;
+import com.sem.keeper.repo.UserRepository;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,6 +17,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +29,21 @@ public class AjaxController {
 
     @Autowired
     DeviceRepository devices;
+
+    @Autowired
+    LoanRepository loanRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    LoanRequestRepository loanRequestRepository;
+
+    @GetMapping("/myReqs")
+    public Iterable<LoanRequestEntity> myReqs(HttpSession session){
+        UserEntity user = (UserEntity) session.getAttribute("user");
+        return loanRequestRepository.findByElvinne(user);
+    }
 
     @GetMapping("/devices")
     public Iterable<DeviceEntity> devicePag(@RequestParam int page, @RequestParam int pageSize){
@@ -36,6 +58,30 @@ public class AjaxController {
         Integer page = pageNumber.orElse(0);
         var pageable = PageRequest.of(page,pageSize.orElse(10));
         return devices.findByNameContainingIgnoreCase(term, pageable);
+    }
+
+    @GetMapping(value = {"/loanlist","/loanlist/{pageNumber}/{pageSize}"})
+    public Page<LoanEntity> loanlist(HttpSession session,
+                                     @RequestParam Optional<String> filter,
+                                     @PathVariable Optional<Integer> pageNumber,
+                                     @PathVariable Optional<Integer> pageSize){
+        Pageable pageable = PageRequest.of(pageNumber.orElse(0),pageSize.orElse(10));
+        if (filter.isEmpty() || filter.get().equals("all")) {
+            return loanRepository.findAll(pageable);
+        } else {
+            switch (filter.get()){
+                case "mine":{
+                    UserEntity user = (UserEntity) session.getAttribute("user");
+                    return loanRepository.findByElvitte(user, pageable);
+                }
+                case "out":{
+                    return loanRepository.findByVisszavetteIsNullOrderByTakeDate(pageable);
+                }
+                default:{
+                    return loanRepository.findAll(pageable);
+                }
+            }
+        }
     }
 }
 
