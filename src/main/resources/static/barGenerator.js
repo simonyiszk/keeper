efac740be08e97eccfb9a2d6f975f0050950bf25
varkokonyi <varkokonyi@gmail.com>
2020-07-02 17:44:08 +0200

    Advanced bars

--- src/main/resources/static/barGenerator.js
+++ src/main/resources/static/barGenerator.js
@@ -6,6 +6,7 @@ function makeBars() {
         let theProgress = document.createElement("div")
         theProgress.classList.add("progress-bar")
         theDiv.appendChild(theProgress)
+        theProgress.innerText=moment(rows[i].dataset['deadline']).fromNow()
         theProgress.role="progressbar"
         let taken = Date.parse(rows[i].dataset['taken'])
         let deadline = Date.parse(rows[i].dataset['deadline'])
@@ -14,10 +15,17 @@ function makeBars() {
         theProgress.style.width=percent
         let theTr=document.createElement("tr")
         let theTd=document.createElement("td")
+        theTr.title=`Elvitte: ${moment(rows[i].dataset['elvitte']).format('YYYY-MM-DD hh:mm')}<br>Határidő: ${moment(rows[i].dataset['deadline']).format('YYYY-MM-DD hh:mm')}<br>${parseInt(percent)}%`
+        theTr.dataset['placement']="auto"
+        theTr.dataset['html']="true"
+        theTr.dataset['toggle']="tooltip"
         theTr.appendChild(theTd)
         theTd.appendChild(theDiv)
-        theTd.colSpan=6
+        theTd.colSpan=69
         rows[i].parentNode.insertBefore(theTr, rows[i].nextSibling);
 
     }
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip();
+    });
 }
